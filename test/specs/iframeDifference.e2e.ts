4d841808a3c921444a76816052e7b0d7ac2512ed
Hauke Thorenz <hauke.thorenz@weidmueller.com>
2025-03-18 15:36:02 +0100

    add iframeDifference

---
+++ test/specs/iframeDifference.e2e.ts
@@ -0,0 +1,26 @@
+
+import { expect, browser } from '@wdio/globals'
+
+describe(`iframeDifference`, () => {
+    it.only("debug-time", async () => {
+        await browser.url(`https://htho.github.io/wdio-repro-collection/iframeDifference.html`);
+
+        await browser.debug();
+    });
+    it("p is added by custom element (fails when contentWindow is logged)", async () => {
+        await browser.url(`https://htho.github.io/wdio-repro-collection/iframeDifference.html`);
+
+        await browser.switchFrame("https://htho.github.io/wdio-repro-collection/iframeDifferenceInner.html");
+        await expect($("my-element")).toExist();
+        await expect($("my-element p")).toExist(); // <-- fails
+    });
+    it("p is added by custom element", async () => {
+        await browser.url(`https://htho.github.io/wdio-repro-collection/iframeDifference.html?noLog`);
+
+        await browser.switchFrame("https://htho.github.io/wdio-repro-collection/iframeDifferenceInner.html");
+        await expect($("my-element")).toExist();
+        await expect($("my-element p")).toExist();
+    });
+});
+
+
