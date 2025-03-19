ffcd107ac624c1e77b511cdc7f83d8be9ef31295
Hauke Thorenz <hauke.thorenz@weidmueller.com>
2025-03-19 10:15:10 +0100

    log elementId

--- test/specs/multipleElements.e2e.ts
+++ test/specs/multipleElements.e2e.ts
@@ -4,6 +4,7 @@ import { expect, browser, $$ } from '@wdio/globals'
 describe('$$', () => {
     it(`counts (fails when a custom element is used and defined)`, async () => {
         await browser.url(`https://htho.github.io/wdio-repro-collection/multipleElements.html`)
+        console.log(await $$("p").map(e=> e.elementId));
         await expect($$("p")).toBeElementsArrayOfSize(2);
     });
     it(`counts (ok if a used custom element stays not defined)`, async () => {
