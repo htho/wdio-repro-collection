666e4e77eac75f19bb360cb886b1461a7a37cb5d
Hauke Thorenz <hauke.thorenz@weidmueller.com>
2025-03-19 10:10:48 +0100

    add multipleElements

---
+++ test/specs/multipleElements.e2e.ts
@@ -0,0 +1,14 @@
+
+import { expect, browser, $$ } from '@wdio/globals'
+
+describe('$$', () => {
+    it(`counts (fails when a custom element is used and defined)`, async () => {
+        await browser.url(`https://htho.github.io/wdio-repro-collection/multipleElements.html`)
+        await expect($$("p")).toBeElementsArrayOfSize(2);
+    });
+    it(`counts (ok if a used custom element stays not defined)`, async () => {
+        await browser.url(`https://htho.github.io/wdio-repro-collection/multipleElements.html?noCustom`)
+        await expect($$("p")).toBeElementsArrayOfSize(2);
+    });
+});
+
